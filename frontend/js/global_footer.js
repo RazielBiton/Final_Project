b8184aa8bf7e807a3fa55895246d1ec2b3195c9c
Raziel Biton <razielbiton7@gmail.com>
2026-07-08 11:48:57 +0300

    update footer ui

--- frontend/js/global_footer.js
+++ frontend/js/global_footer.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const footerHtml = `
-        <p class = "footer-text">Final Project - EasyCare Team for Yizrael Valley College 2026 - Raziel Biton & Michael Geyshes</p>
-        <button class="footer-contact" onclick="window.location.href='contact.html'">contact us</button>
-    `;
+    <p class = "footer-text">Final Project - EasyCare Team<br>Raziel Biton & Michael Geyshes</p>
+    <button class="footer-contact" onclick="window.location.href='contact.html'">contact us</button>`;
 
     // Find the global footer container on the page
     const footerContainer = document.querySelector(".global-footer");
-
     if (footerContainer) {
         footerContainer.innerHTML = footerHtml;
     }
